7cb657647690cf269f9ad35ee30dde90cec4ad18
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-29 09:40:02 -0400

    Added doc bloks and additional checks for the addItem function

--- src/js/inventory.controller.js
+++ src/js/inventory.controller.js
@@ -25,7 +25,11 @@
       { "id": 683, "name": "pillow", "price": 27, "quantity": 10, "color": "black", "discount": 12 }
     ];
 
-
+    /**
+     * Calculates the final price for an individual item
+     * @param  {Array} item Should have price and discount for this function to work
+     * @return {Object}     The new calculated price including the price, discount, and taxt rate.
+     */
     vm.finalizePrice = function finalizePrice(item) {
       let discountPrice = item.price - item.discount;
       vm.tax = tax;
@@ -33,10 +37,20 @@
       return taxPlusPrice;
     };
 
+    /**
+     * Adds a new item to the array of items
+     * @param {Object} item Requires a name, price, quantity, color, and discount
+     * @return {void}
+     */
     vm.addItem = function addItem(item) {
-      if (typeof(item) !== 'object' || typeof(item.name) !== 'string') {
+      if (typeof(item) !== 'object' || typeof(item.name) !== 'string' || item.name.length < 1) {
         return;
       }
+      item.price = Number(item.price);
+      if (Number.isNaN(item.price)) {
+        return;
+      }
+      //if there is no quantity or discount set them to 0
 
       vm.items.push({
         name: item.name,
