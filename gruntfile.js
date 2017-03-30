6c7c107c39667024dfa93241f990ea075e3d3a1e
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-30 14:58:22 -0400

    Installed angular

--- gruntfile.js
+++ gruntfile.js
@@ -10,6 +10,7 @@ module.exports = function(grunt) {
           frameworks: [ 'mocha', 'chai' ],
           browsers: [ 'Chrome' ],
           files: [
+            'node_modules/angular/angular.js',
             'src/js/shopular.module.js',
             'src/js/**/*.js',
             'test/**/*.spec.js'
