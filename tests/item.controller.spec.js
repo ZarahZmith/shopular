4948c6d3b11d5b3036df922d7267847dcb2592b0
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-31 15:58:19 -0400

    Changed the name of file to be consistent with other naming

---
+++ tests/item.controller.spec.js
@@ -0,0 +1,46 @@
+(function() {
+  'use strict';
+
+  let expect = chai.expect;
+
+  describe('inventory controller', function() {
+
+    let ItemController;
+    let mockShopService = {};
+
+    beforeEach(module('shopular'));
+
+    beforeEach(module(function($provide) {
+      $provide.value('ShopService', mockShopService);
+    }));
+
+    beforeEach(inject(function($controller) {
+      mockShopService.getAllItems = function getAllItems() {
+        let now = Date.now();
+        return [{
+          id: now,
+          name: 'wallet',
+          price: 350.99,
+          quantity: 45,
+          color: 'black',
+          discount: 55
+        }];
+      };
+      mockShopService.createItem = function createItem(argOne) {
+        mockShopService.createItem.numTimesCalled++;
+        return;
+      };
+      mockShopService.createItem.numTimesCalled = 0;
+
+      ItemController = $controller('ItemController');
+    }));
+
+    it('should have all the things we expect it to', function() {
+      expect(ItemController).to.be.an('object');
+    });
+
+    //TODO describes to test each individual fn
+
+  });
+
+}());
