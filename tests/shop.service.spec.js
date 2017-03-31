1cf71f568e994d37e21e2690a2c1f02e2a9bbd12
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-31 12:10:19 -0400

    Created test for when a price below one cent was added. This requied adding an afterEach in the test file to clear out localStorage after evry test was run

--- tests/shop.service.spec.js
+++ tests/shop.service.spec.js
@@ -13,6 +13,10 @@
       ShopService = _ShopService_;
     }));
 
+    afterEach(function() {
+      localStorage.removeItem('items');
+    });
+
     describe('get data function', function() {
       it('should be able to get an array of all items', function() {
         let result = ShopService.getAllItems();
@@ -38,6 +42,20 @@
         expect(item[0].quantity).to.equal(55);
         expect(item[0].color).to.equal('orange');
         expect(item[0].discount).to.equal(0);
+      });
+
+      it('should get no item if the price is below 1 cent', function() {
+        expect(ShopService.getAllItems().length).to.equal(0);
+        let now = Date.now();
+        ShopService.createItem({
+          name: 'Hammer' + now,
+          price: 0,
+          quantity: 2,
+          color: 'silver',
+          discount: 0
+        });
+        let item = ShopService.getAllItems();
+        expect(item.length).to.equal(0);
       });
     });
 
